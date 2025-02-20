7cde48036bdf1e5b075c0bfa355185aa28afb0c5
nirajajshenoy <157449412+nirajajshenoy@users.noreply.github.com>
2025-02-20 15:51:18 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/nirajajshenoy/Go-Backend/controllers"
+	"github.com/nirajajshenoy/go_backend/controllers"
 )
 
 func main() {
